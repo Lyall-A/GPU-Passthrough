9b833542eac9f2e57cd01e53680d8895dabf34a1
Lyall <58636255+Lyall-A@users.noreply.github.com>
2024-07-24 09:24:14 +0100

    Update grub_setup_intel.sh

--- grub_setup_intel.sh
+++ grub_setup_intel.sh
@@ -1,19 +1,17 @@
-if [ $EUID -ne 0 ]
-    then
-        echo "This program must run as root to function." 
-        exit 1
+if [ $EUID -ne 0 ]; then
+    echo "This script must run as root!" 
+    exit 1
 fi
 
-sleep 1s
+sleep 1
 
 echo "Adding 'intel_iommu=on iommu=pt' to GRUB config..."
 GRUB=`cat /etc/default/grub | grep "GRUB_CMDLINE_LINUX_DEFAULT" | rev | cut -c 2- | rev`
 GRUB+=" intel_iommu=on iommu=pt\""
 sed -i -e "s|^GRUB_CMDLINE_LINUX_DEFAULT.*|${GRUB}|" /etc/default/grub
-sleep 1s
+sleep 1
 clear
 
 echo "Making GRUB config..."
 grub-mkconfig -o /boot/grub/grub.cfg
-sleep 5s
-exit
+sleep 5
